fe03a109080ef24033cfca23e6e7103195d24acc
Aryan Patel <adp2582@gmail.com>
2021-09-25 22:08:53 -0400

    deploying to heroku

--- src/App.js
+++ src/App.js
@@ -1,24 +1,265 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/MusicPortal.json";
+import { ChakraProvider } from "@chakra-ui/react";
+import { VStack, Grid, Heading, Box, Spinner } from "@chakra-ui/react";
+import { Button, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FaLink } from "react-icons/fa";
+
+//components 
+import Song from "./components/Song";
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = React.useState("");
+  const [allSongs, setAllSongs] = React.useState([]);
+  const [formValue, setFormValue] = React.useState("");
+  const [mining, setMining] = React.useState(false);
+
+  const contractAddress = "0xefadB6d5bbdE92475E0E5919d1ab5628ed0941F4";
+  const contractABI = abi.abi;
+
+  React.useEffect(() => { //runs when app is launched 
+    walletConnected();
+  }, []);
+
+
+  const sendSong = async () => {
+
+    if(formValue === "") {
+      alert("please enter a spotify link!");
+      return;
+    }
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const musicportalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const sendTxn = await musicportalContract.wave(formValue);
+        console.log("Mining... -- Hash: ", sendTxn.hash);
+        setMining(true);
+
+        await sendTxn.wait();
+        console.log("Mined -- Hash: ", sendTxn.hash);
+        setMining(false);
+
+        getAllSongs();
+    
+      } else {
+        console.log("Ethereum object doesn't exist in current window.");
+      }
+
+    } catch (error) {
+      console.log(error); 
+
+    }
+
+  }
+
+  const getAllSongs = async () => {
+
+    try {
+
+      const { ethereum } = window;
+
+      if (ethereum) {
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const musicportalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const songs = await musicportalContract.getAllSongWaves();
+
+        let songsCleaned = [];
+        songs.forEach(SongWave => {
+          songsCleaned.push({
+            address: SongWave.waver,
+            timestamp: new Date(SongWave.timestamp * 1000),
+            link: SongWave.link
+
+          });
+
+        });
+
+        setAllSongs(songsCleaned);
+
+      } else {
+        console.log("Ethereum object doesn't exist!")
+
+      }
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  }
+
+  const walletConnected = () => {
+
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask Wallet!");
+      return;
+
+    } else {
+      console.log("Ethereum object found: ", ethereum);
+
+    }
+
+    ethereum.request({ method: 'eth_accounts' })
+      .then(accounts => {
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account: ", account);
+
+          setCurrentAccount(account);
+          getAllSongs();
+          
+        } else {
+          console.log("No authorized account found.");
+
+        }
+
+      })
+
+  }
+
+  const connectWallet = () => {
+
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Metamask Wallet is required to use this Web App!");
+    }
+
+    ethereum.request({ method: "eth_requestAccounts" })
+      .then(accounts => {
+        console.log("Connected!", accounts[0]);
+        setCurrentAccount(accounts[0]);
+
+        getAllSongs();
+
+      }).catch(err => console.log(err));
+
+  }
+
+  const handleChange = (event) => setFormValue(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+
+      <VStack
+        p={4} 
+        w='100%' 
+        alignItems='center' 
+        spacing='20px'>
+
+        < br />
+
+        <Heading>Welcome to Aryan's Wall of Music!</Heading>
+
+        <br />
+
+        <Box
+          w='60%' 
+          borderColor='black' 
+          borderWidth='3px' 
+          p='4' 
+          borderRadius='lg' 
+          color='black'
+          fontWeight='semibold'>
+
+          Hi 👋🏽👋🏽!! My name is Aryan and I love music! Connect your wallet and send me your favorite tune through a Spotify share link. Each time you send a song, you might have the chance to win some 
+          free Ethereum!! Feel free to check out all the songs sent so far down below. Thank you and stay jammin!
+        </Box>
+
+        <br />
+
+        {mining ? ([ <Spinner size="xl" />, < br /> ]) : null}
+
+        {currentAccount ? (
+
+          [ <Button       
+            p='4'
+            h='65px'
+            w= '40%'
+            size='lg' 
+            color='white' 
+            borderColor='black' 
+            borderWidth='3px'
+            bgGradient='linear(to-l, #29323c, #485563)' 
+            _hover={{
+              bgGradient: 'linear(to-t, #43e97b, #38f9d7)',
+            }}
+            onClick={sendSong}>
+            
+            Send Song!
+          </Button>,
+
+          <InputGroup alignSelf='center' w='40%' >
+             <InputLeftElement
+              pointerEvents='none'
+              children={<FaLink />}/>
+            ` <Input placeholder="paste spotify link here"
+                    borderColor='black'
+                    focusBorderColor='#43e97b' 
+                    borderWidth='3px'
+                    value={formValue}
+                    onChange={handleChange} />
+          </InputGroup> ]
+
+        ) : (
+  
+          <Button       
+            p='4'
+            h='65px'
+            w= '40%'
+            size='lg' 
+            color='white' 
+            borderColor='black' 
+            borderWidth='3px'
+            bgGradient='linear(to-l, #29323c, #485563)' 
+            _hover={{
+              bgGradient: 'linear(to-r, #43e97b, #38f9d7)',
+            }}
+            onClick={connectWallet}>
+
+            Connect MetaMask Wallet
+          </Button>
+
+        )}      
+
+      </VStack>
+
+      <br />
+
+      <VStack
+         p={4}  
+         w='100%'
+         alignItems='center' 
+         spacing='20px'>
+
+        <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+          {allSongs.map((song, index) => {
+            return (
+                <Song url={song.link} address={song.address} timestamp={song.timestamp} />
+              )
+          })}
+        </Grid>
+
+      </VStack>
+
+    </ChakraProvider>
+    
+
   );
 }
 
---
+++ src/components/Song.js
@@ -0,0 +1,128 @@
+import React from "react";
+import axios from "axios";
+import { Box, Center, useColorModeValue, Heading, Text, Stack, Image, Link } from '@chakra-ui/react';
+import { encode as base64_encode } from 'base-64';
+
+
+const Song = ({url, address, timestamp}) => {
+    
+    const [image, setImage] = React.useState("https://via.placeholder.com/300");
+    const [artistName, setArtistName] = React.useState("default");
+    const [songName, setSongName] = React.useState("default");
+
+    const conv_time = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', 
+                            hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp); 
+
+    console.log(conv_time);
+
+    React.useEffect(() => { 
+
+        var id = new URL(url).pathname.split('/')[2];
+    
+        //make request for Oauth Acess Token, then make request for song info
+        axios('https://accounts.spotify.com/api/token', {
+            headers: {
+              'Content-Type' : 'application/x-www-form-urlencoded',
+              'Authorization' : 'Basic ' + base64_encode(process.env.REACT_APP_CLIENT_ID + 
+                    ':' + process.env.REACT_APP_CLIENT_SECRET)     
+            },
+            data: 'grant_type=client_credentials',
+            method: 'POST'
+
+          }).then(tokenResponse => {      
+            
+            axios('https://api.spotify.com/v1/tracks/' + id , {
+                headers: {
+                    'Authorization' : 'Bearer ' + tokenResponse.data.access_token
+                },
+                method: 'GET'
+
+            }).then(dataResponse => {
+
+                setSongName(dataResponse.data.name);
+                setArtistName(dataResponse.data.artists[0].name);
+                setImage(dataResponse.data.album.images[1].url);
+                
+            });
+
+          });      
+
+    }, []);
+
+    return (
+
+        <Center py={12}>
+            <Box
+                role={'group'}
+                p={6}
+                maxW={'460px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                zIndex={1}>
+                <Box
+                rounded={'lg'}
+                mt={-12}
+                pos={'relative'}
+                height={'290px'}
+                _after={{
+                    transition: 'all .3s ease',
+                    content: '""',
+                    w: 'full',
+                    h: 'full',
+                    pos: 'absolute',
+                    top: 5,
+                    left: 0,
+                    backgroundImage: `url(${image})`,
+                    filter: 'blur(15px)',
+                    zIndex: -1,
+                }}
+                _groupHover={{
+                    _after: {
+                    filter: 'blur(20px)',
+                    },
+                }}>
+
+                <Link href={url} isExternal>
+                    <Image
+                        rounded={'lg'}
+                        height={300}
+                        width={300}
+                        objectFit={'cover'}
+                        src={image}
+                    
+                    />
+                </Link>
+              
+                </Box >
+
+                <Stack pt={10} align={'center'}>
+                    
+                    <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+                        {artistName}
+                    </Text>
+                    <Text fontSize={'2xl'} fontFamily={'body'} fontWeight={500} maxW={'300px'} align={'center'} >
+                        {songName}
+                    </Text>
+
+                    <br />
+
+                    <Stack direction={'column'} align={'center'}>
+                        <Text color={'gray.600'}>
+                        {conv_time}
+                        </Text>
+                        <Text color={'gray.600'} fontSize='9px' href={url}>
+                        {address}
+                        </Text>
+                    </Stack>
+                </Stack>
+            </Box>
+        </Center>
+
+    );
+
+}
+
+export default Song
